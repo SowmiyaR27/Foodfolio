461c7cc62914942b5c9bb63f00570e366e63fb2d
Sowmiya R <sowmiyaramesh1127@gmail.com>
2025-04-17 19:39:51 +0530

    Delete Simple Website directory

--- Simple Website/index.js
+++
@@ -1,9 +0,0 @@
-//JavaScript code for toggle menu 
-
-var navLinks = document.getElementById("navLinks");
-        function showMenu(){
-            navLinks.style.right = "0";
-        }
-        function hideMenu(){
-            navLinks.style.right = "-200px";
-        }
